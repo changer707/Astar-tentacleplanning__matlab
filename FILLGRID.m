57f531c7270c04132a05b7c6e67c9be3fad67370
changer707 <53569122+changer707@users.noreply.github.com>
2019-09-24 11:19:22 +0800

    Delete FILLGRID.m

--- FILLGRID.m
+++
@@ -1,5 +0,0 @@
-function FILLGRID(obstacle,color)
-for i=1:length(obstacle(:,1))
-    fill([obstacle(i,1)-0.5,obstacle(i,1)-0.5,obstacle(i,1)+0.5,obstacle(i,1)+0.5],[obstacle(i,2)+0.5,obstacle(i,2)-0.5,obstacle(i,2)-0.5,obstacle(i,2)+0.5],color);
-end
-end
